1d2ae040c73fadc9d10a11e50b5aaf2d0af19c24
Kelechi <ekelechi413@gmail.com>
2026-05-02 12:38:12 +0100

    done

---
+++ backend/upload.js
@@ -0,0 +1,7 @@
+const multer = require("multer");
+const { cloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
+
+const storage = cloudinaryStorage({
+    
+})
